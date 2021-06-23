295421f382f43e5fe5754f64b20dd2b813d67738
oahwin <oahwin@getcarbon.co>
2021-06-23 21:38:41 +0100

    removed unnecessary import

--- src/main/java/com/rukevwe/exchange/rate/provider/coindesk/CoindeskService.java
+++ src/main/java/com/rukevwe/exchange/rate/provider/coindesk/CoindeskService.java
@@ -4,7 +4,6 @@ import com.rukevwe.exchange.rate.model.RateInfo;
 import com.rukevwe.exchange.rate.model.RateList;
 import com.rukevwe.exchange.rate.provider.RateProviderService;
 import com.rukevwe.exchange.rate.provider.coindesk.models.RateResponse;
-import com.rukevwe.exchange.rate.service.RateService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
